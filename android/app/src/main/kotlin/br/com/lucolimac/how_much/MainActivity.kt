855f0d04f74ab589894388960bc0c54e7995c56a
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2023-11-28 23:25:26 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/br/com/lucolimac/how_much/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.lucolimac.how_much
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'How much time? ❤️'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _startDate = DateTime(2023, 1, 2, 0, 0, 0);
+  final _actualDate = DateTime.now();
+  Duration difference = Duration.zero;
+
+  void _setTime() {
+    setState(() {
+      difference = _actualDate.difference(_startDate);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    wh
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      _setTime();
+    });
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            const Text(
+              "Since",
+              style: TextStyle(fontWeight: FontWeight.w800, fontSize: 26),
+            ),
+            Text(
+              DateFormat.yMMMMd('en_US').format(_startDate),
+              style: const TextStyle(fontWeight: FontWeight.w700, fontSize: 24),
+            ),
+            Text("${difference.inDays} days"),
+            Text("${difference.inHours} hours"),
+            Text("${difference.inMinutes} minutes"),
+            Text("${difference.inSeconds} seconds"),
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
